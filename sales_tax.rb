b2c019697bb073e15154e0ae91b5cf6272a36286
shari-singh <shari.singh93@gmail.com>
2016-06-06 10:52:06 -0400

    fixed array error. need to fix tax_calc method

--- sales_tax.rb
+++ sales_tax.rb
@@ -1,40 +1,54 @@
-class Items
-  attr_accessor :name, :price, :import_tax, :sales_tax
+class Item
+  attr_accessor :name, :price, :import_tax, :sales_tax, :total
   def initialize (name, price, import_tax, sales_tax)
     @name = name
     @price = price
+    @total
     @import_tax = import_tax
     @sales_tax = sales_tax
   end
 end
 
 class Receipt
-  attr_reader :item_list, :tax_total, :receipt_total
+  attr_reader :receipt_total, :before_tax, :sum
 
   def initialize
     @total_price = []
     @item_list = []
+    @total = 0
     @receipt_total = 0
     @sum = 0
     @before_tax = []
   end
 
+  def add_item(item)
+    if item != nil
+      @item_list << item
+    end
+  end
 
   def item_tax
     @item_list.each do |item|
-    if (item[2] == 'true' && import_tax == 'true')  #charge both import and sales tax if both are true
-      # @total += @price * (0.10 + 0.05)
-      "1 #{item.name} : #{((@total + item.price)*(0.10+0.05)).round(2)}"
-    elsif (item[2] == 'false' && import_tax == 'true') #charge only import tax
-      # @total += @price * 0.05
-      "1 #{item.name} : #{((@total + item.price)*(0.05)).round(2)}"
-    elsif (item[2] == 'true' && import_tax == 'false') #charge only sales tax
-      @total += @price * 0.10
-      puts "1 #{item.name} : #{((@total + item.price)*(0.10)).round(2)}"
-    elsif (item[2] == 'false' && import_tax == 'false') #no tax charge
-      puts "1 #{item.name} : #{@total.round(2)}"
+    if (item.sales_tax == 'true' && import_tax == 'true')  #charge both import and sales tax if both are true
+      item.total += item.price * (0.10 + 0.05)
+
+    elsif (item.sales_tax == 'false' && import_tax == 'true') #charge only import tax
+      item.total += item.price * 0.05
+
+    elsif (item.sales_tax == 'true' && import_tax == 'false') #charge only sales tax
+      item.total += item.price * 0.10
+
+    elsif (item.sales_tax == 'false' && import_tax == 'false') #no tax charge
+      item.price
+
     end
   end
+  end
+
+  def list
+    @item_list.each do |items|
+      puts "puts 1 #{items.name}: $#{items.price}"
+    end
   end
 
   def add(item)
@@ -46,41 +60,56 @@ class Receipt
     @before_tax.each do |x|
       @sum += x
     end
-    
-      @total_price.each do |y|
+
+    @total_price.each do |y|
       @receipt_total += x
     end
   end
 
   def totals
-    puts "Sales taxes:#{@sum.round(2)}"
-    puts "Total included taxes: #{@receipt_total.round(2)}"
+    puts "Sales taxes:#{@sum.to_f/20}"
+    puts "Total included taxes: #{@receipt_total.to_f/20}"
   end
 end
 
-book = Items.new("Book", 12.49, 'false', 'false')
-music_cd = Items.new("Music CD", 14.99, 'true', 'false')
-choco_bar = Items.new("Chocolate Bar", 0.85, 'false', 'false')
-import_choco_box = Items.new("Imported Box of Chocolates", 10, 'false', 'true')
-import_perfume = Items.new("Imported Bottle of Perfume", 47.50, 'true', 'true')
-import_perfume2 = Items.new("Imported Bottle of Perfume", 32.19, 'true', 'true')
-perfume2 = Items.new("Bottle of Perfume", 20.89, 'true', 'false')
-headache_pills = Items.new("Packet of Headache Pills", 9.75, 'false', 'false')
-import_choco_box2 = Items.new("Imported Box of Chocolates", 11.85, 'false', 'true')
-
-receipt_items1 = []
-receipt_items2 = []
-receipt_items3 = []
-
-receipt_items1 << [book, music_cd, choco_bar]
-receipt_items2 << [import_choco_box, import_perfume]
-receipt_items3 << [import_perfume2, perfume2, headache_pills, import_choco_box2]
-
 receipt1 = Receipt.new
-receipt1.item_tax
-receipt1.sum_price
-receipt1.totals
-# receipt1.print_items
-
 receipt2 = Receipt.new
 receipt3 = Receipt.new
+
+music_cd = Item.new("Music CD", 14.99, true, false)
+choco_bar = Item.new("Chocolate Bar", 0.85, false, false)
+import_choco_box = Item.new("Imported Box of Chocolates", 10, false, true)
+import_perfume = Item.new("Imported Bottle of Perfume", 47.50, true, true)
+import_perfume2 = Item.new("Imported Bottle of Perfume", 32.19, true, true)
+perfume2 = Item.new("Bottle of Perfume", 20.89, true, false)
+headache_pills = Item.new("Packet of Headache Pills", 9.75, false, false)
+import_choco_box2 = Item.new("Imported Box of Chocolates", 11.85, false, true)
+
+book = Item.new("Book", 12.49, false, false)
+
+puts "-------- Receipt 1 ------------"
+receipt1.add_item(book)
+receipt1.add_item(music_cd)
+receipt1.add_item(choco_bar)
+receipt1.item_tax
+receipt1.list
+receipt1.sum_price
+receipt1.totals
+
+puts "-------- Receipt 2 ------------"
+receipt2.add_item(import_choco_box)
+receipt2.add_item(import_perfume)
+receipt2.item_tax
+receipt2.list
+receipt2.sum_price
+receipt2.totals
+
+puts "-------- Receipt 3 ------------"
+receipt3.add_item(import_perfume2)
+receipt3.add_item(perfume2)
+receipt3.add_item(headache_pills)
+receipt3.add_item(import_choco_box2)
+receipt3.item_tax
+receipt3.list
+receipt3.sum_price
+receipt3.totals
