3acc139b1a22407d22c60c01c8ebc6feda89401e
shari-singh <shari.singh93@gmail.com>
2016-06-05 21:25:53 -0400

    updated comments

--- mars_rover/rover.rb
+++ mars_rover/rover.rb
@@ -11,30 +11,30 @@ class Rover
   def read_instr(instr_sequence)
     instr_sequence.each do |instruction| #temp value to hold each individual instruction
       if instruction == "L"
-        self.turn_left(instruction)
+        self.turn_left(instruction)  #uses turn_left method if it's Left
       elsif instruction == "R"
-        self.turn_right(instruction)
+        self.turn_right(instruction) #uses turn_right method if it's Right
       elsif instruction == "M"
-        self.move_rover
+        self.move_rover  #uses move_rover method if prompted
       end
     end
   end
 
   #--------- MOVING UP AND DOWN  --------------
-    def move_rover
-    if @direction == 'N'
+    def move_rover # moves forward depending on the direction
+    if @direction == 'N' #------NORTH
       @y += 1
-    elsif @direction == 'S'
+    elsif @direction == 'S' #----SOUTH
       @y -= 1
-    elsif @direction == 'E'
+    elsif @direction == 'E' # ---- EAST
       @x += 1
-    else #--- W
+    else #--- ------------- WEST
       @x -= 1
     end
   end
   #----------- DETERMINE DIRECTION --------------------
 
-  def turn_left(l_dir) # ------turns rover left
+  def turn_left(l_dir) # ------turns rover left and updates the direction
     case @direction
     when 'N' #------ NORTH
       @direction = 'W'
@@ -48,23 +48,23 @@ class Rover
   when 'E'  #------- EAST
       @direction = 'N'
     end
-  end #---------------END of turn_left method
+  end
 
-  def turn_right(r_dir) # ----------- turns rover right
+  def turn_right(r_dir) # ----------- turns right and updates direction
     case @direction
-    when 'N'
+    when 'N' #---------- NORTH
       @direction = 'E'
 
-    when 'E'
+    when 'E' #---------- EAST
       @direction = 'S'
 
-    when 'S'
+    when 'S' #---------- SOUTH
       @direction = 'W'
 
-    when 'W'
+    when 'W' #---------- WEST
       @direction = 'N'
     end
-  end #------------ END of turn_right
+  end
 
   def check_position
     "#{@name}position : #{@x},#{@y} #{@direction}"
--- mars_rover/test.rb
+++ mars_rover/test.rb
@@ -1,23 +1,24 @@
 require_relative 'rover.rb'
 require_relative 'plateau.rb'
-
+#-------------------- INITIALIZE PLATEAU ------------------------------
 puts "Plateau size:"
 plateau_size = gets.chomp.split(" ")
 plateau1 = Plateau.new(plateau_size)
 
+#---------------------INITIALIZE ROVERS -------------------------------
 puts "How many rovers are there?"
 rover_num = gets.chomp.to_i
-rovers = []
+rovers = [] #---------empty array to store rovers
 
-rover_num.times do |num|
+rover_num.times do |num| #makes the amount of rovers specified
   n= 0
   puts "What's rover #{n}'s name?"  #getting the rover's name
   rover_name = gets.chomp
 
   puts "Where is #{rover_name}? X  Y  Direction"  #getting the rover's starting position
-  pos_1 = gets.chomp.split(" ")
+  pos_1 = gets.chomp.split(" ")  #splits position into an array
   new_rover = Rover.new(rover_name, pos_1)
-  rovers << new_rover
+  rovers << new_rover  # puts the new rover in an array
   n+=1
 end
 
@@ -29,17 +30,17 @@ end
     n+=1
   end
 
-  rover_id = gets.chomp.to_i
+  rover_id = gets.chomp.to_i  #----------gets the position of the rover in the array
   puts current_rov = rovers[rover_id]
 
   puts "Give #{rover_id} #{current_rov.name} a command"
   puts "'L' to turn left,  'R' to turn right,  'M' to move forward and Q to Quit"
 
-  5.times do
+  5.times do  #------takes 5 commands to move the rover
     instr_sequence = gets.chomp.upcase.split("") #splits into array of strings
     current_rov.read_instr(instr_sequence)
-    puts current_rov.check_position
-    if instr_sequence.length >= 5
+    puts current_rov.check_position  # ---- shows an updated position after every command
+    if instr_sequence.length >= 5  #----- exits if the user puts in all 5 commands in the first line
       exit
     end
   end
