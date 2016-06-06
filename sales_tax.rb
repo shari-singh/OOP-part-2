2d4051b3028a8657d0cbb32f378bd03ad6a36f36
shari-singh <shari.singh93@gmail.com>
2016-06-05 22:32:39 -0400

    Delete sales_tax.rb

--- sales_tax.rb
+++
@@ -1,9 +0,0 @@
-class Item
-  attr_accessor :name, :num, :start_price, :taxes
-  def initilize (name, num, start_price)
-    @taxes = 0
-    @name = name
-    @num = num
-    @start_price = start_price
-  end
-end
